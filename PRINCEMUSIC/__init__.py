85cd82a089953430e363cb79c2ecd3006512f640
STRANGER <147219905+itzshukla@users.noreply.github.com>
2025-01-17 20:19:25 +0530

    Add files via upload

--- PRINCEMUSIC/__init__.py
+++ PRINCEMUSIC/__init__.py
@@ -1,1 +1,30 @@
+from PRINCEMUSIC.core.bot import PRINCE
+from PRINCEMUSIC.core.dir import dirr
+from PRINCEMUSIC.core.git import git
+from PRINCEMUSIC.core.userbot import Userbot
+from PRINCEMUSIC.misc import dbb, heroku
 
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = PRINCE()
+userbot = Userbot()
+api = SafoneAPI()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+
+APP = "ll_DRAGON_MUSIC_BOT"  # connect music api key "Dont change it"
