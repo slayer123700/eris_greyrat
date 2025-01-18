972869748580d9546c1934080aae1648c152b27d
STRANGER <147219905+itzshukla@users.noreply.github.com>
2025-01-18 17:49:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RDXSHUKLA/MAHIRA-MUSIC",
+    "https://github.com/itzshukla/PRINCE-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
